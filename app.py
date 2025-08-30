4f6b805fb30d060ffe44771dba83811cac639594
TaeWoonPark <yasuoa19@gmail.com>
2025-08-30 11:06:33 +0900

    app.py修正

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ load_dotenv(override=True)
 
 app = Flask(__name__)
 
-line_bot_api = line_bot_api(os.environ["ACCESS_TOKEN"])
-handler = webHookHandler(os.environ["CHANNEL_SECRET"])
-
+line_bot_api = LineBotApi(os.environ["ACCESS_TOKEN"])
+handler = WebhookHandler(os.environ["CHANNEL_SECRET"])
 
 
 @app.route("/")
 def index():
     return "You call index()"
+
 
 @app.route("/callback", methods=["POST"])
 def callback():
@@ -24,21 +24,21 @@ def callback():
     body = request.get_data(as_text=True)
 
     app.logger.info("Request body: ", body)
-    
-    
+
     try:
         handler.handle(body, signature)
-    except InvalidSignatureError:    
-        
-    abort(400)
+    except InvalidSignatureError:
+
+        abort(400)
 
     return "OK"
+
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
-if __name__
- == "__main__":
+
+if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
